1c542484bbe5d8d822d236cb18ee426dd8d90cb9
Selvags2020 <38200687+Selvags2020@users.noreply.github.com>
2025-11-04 19:14:10 +0530

    getting ready for prod deployment

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const API_URLS = {
-  BaseURL: 'http://localhost/DaddyChipsAPI/' 
+  // BaseURL: 'http://localhost/DaddyChipsAPI/' 
+   BaseURL: 'https://daddychips.co.in/DaddyChipsAPI/' 
 };
